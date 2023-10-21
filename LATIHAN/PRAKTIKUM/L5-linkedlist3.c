3c6adc3d61ef8b2d29071d68169582b5c64763d2
Gredy Christian <gredy24chris@gmail.com>
2023-10-21 15:20:58 +0700

    linked list simple program example

---
+++ LATIHAN/PRAKTIKUM/L5-linkedlist3.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+//========================================================
+
+struct node {
+	int data;
+    struct node *next;
+};
+typedef struct node node;
+
+//node *createNode(void);
+void tambahAwal(node **head);
+void tambahData(node **head);
+void tranverse(node *head);
+void insertNode(node **head, node *pPre, node *pNew);
+
+//========================================================
+
+int main()
+{
+  node *head;
+  char pilih;
+
+  head = NULL;
+  do{
+     system("cls");
+     printf("masukkan pilihan\n");
+     printf("1. tambah data di awal\n");
+     printf("2. tambah data di tengah list\n");
+     printf("4. cetak isi list\n");
+     printf("MASUKKAN PILIHAN (tekan q untuk keluar) : ");
+     fflush(stdin);
+     scanf("%c", &pilih);
+     if (pilih == '1')
+     		tambahAwal(&head);
+     else if (pilih == '2')
+			tambahData(&head);
+     else if (pilih == '4'){
+     		tranverse(head);
+         getch();
+     }
+  } while (pilih != 'q');
+  
+  return 0;
+}
+
+//========================================================
+
+/*node *createNode(void){
+  node *ptr;
+
+  ptr = (node *)malloc(sizeof(node));
+  return(ptr);
+}*/
+
+//========================================================
+
+void tambahAwal(node **head){
+  int bil;
+  node *pNew;
+
+  system("cls");
+  fflush(stdin);
+  printf("masukkan bilangan : ");
+  fflush(stdin);
+  scanf("%d", &bil);
+  pNew = (node *)malloc(sizeof(node));
+
+  if (pNew != NULL){
+  	  pNew->data = bil;
+      pNew->next = NULL;
+      //add before first logical node or to an empty list
+	  pNew -> next = *head;
+      *head = pNew;
+  }
+  else{
+      printf("Alokasi memori gagal");
+      getch();
+  }
+}
+
+//========================================================
+
+void tambahData(node **head){
+  int pos, bil;
+  node *pNew, *pCur;
+
+  system("cls");
+  tranverse(*head);
+  printf("\nposisi penyisipan setelah data bernilai : ");
+  fflush(stdin);
+  scanf("%d", &pos);
+  printf("\nbilangan : ");
+  fflush(stdin);
+  scanf("%d", &bil);
+
+  pCur = *head;
+  while (pCur != NULL && pCur -> data != pos) {
+    pCur = pCur -> next;
+  }
+
+  pNew = (node *)malloc(sizeof(node));
+
+  if (pCur == NULL){
+     printf("\nnode tidak ditemukan");
+     getch();
+  }
+  else if (pNew == NULL){
+     printf("\nalokasi memeori gagal");
+     getch();
+  }
+  else{
+     pNew->data = bil;
+     pNew->next = NULL;
+     pNew -> next = pCur -> next;
+     pCur -> next = pNew;
+  }
+}
+
+//========================================================
+
+void tranverse(node *head){
+   //traverse a linked list
+	node *pWalker;
+
+   system("cls");
+	pWalker = head;
+	while (pWalker != NULL){
+   	printf("%d\t", pWalker -> data);
+   	pWalker = pWalker -> next;
+      printf(" -> ");
+	}
+   printf("NULL");
+}
+
+//========================================================
+
+void insertNode(node **head, node *pPre, node *pNew){
+    if (pPre == NULL){
+       //add before first logical node or to an empty list
+	    pNew -> next = *head;
+       *head = pNew;
+    }
+    else {
+       //add in the middle or at the end
+       pNew -> next = pPre -> next;
+       pPre -> next = pNew;
+   }
+}
+
+//========================================================
+
