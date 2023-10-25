21ccedda9f4b71d4524afb854f0ee1c79dc59a0d
Gredy Christian <gredy24chris@gmail.com>
2023-10-25 23:07:21 +0700

    new major update on UTS Strukdat cuz I'm stressed out

--- TUGAS/UTS/main.cpp
+++ TUGAS/UTS/main.cpp
@@ -1,286 +1,233 @@
 #include <iostream>
-#include <string>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-// Struktur data untuk mahasiswa
+// Definisi struktur data mahasiswa
 struct Mahasiswa {
     string nama;
     char jenisKelamin;
     Mahasiswa* next;
-
-    Mahasiswa(string _nama, char _jenisKelamin) {
-        nama = _nama;
-        jenisKelamin = _jenisKelamin;
-        next = nullptr;
-    }
 };
 
-// Kelas yang merepresentasikan lingkaran mahasiswa
-class LingkaranMahasiswa {
-private:
-    Mahasiswa* head;
-    Mahasiswa* tail;
+// Fungsi untuk menambahkan mahasiswa ke dalam lingkaran
+void tambahMahasiswa(Mahasiswa*& head, string nama, char jenisKelamin) {
+    Mahasiswa* newMahasiswa = new Mahasiswa;
+    newMahasiswa->nama = nama;
+    newMahasiswa->jenisKelamin = jenisKelamin;
+    newMahasiswa->next = nullptr;
 
-public:
-    LingkaranMahasiswa() {
-        head = nullptr;
-        tail = nullptr;
-    }
-
-    // Menambahkan mahasiswa ke lingkaran
-    void tambahMahasiswa() {
-        string nama;
-        char jenisKelamin;
-
-        cout << "Masukkan nama mahasiswa: ";
-        cin >> nama;
-        cout << "Masukkan jenis kelamin (L/P): ";
-        cin >> jenisKelamin;
-
-        Mahasiswa* newMahasiswa = new Mahasiswa(nama, jenisKelamin);
-
-        if (!head) {
-            head = newMahasiswa;
-            tail = newMahasiswa;
-            newMahasiswa->next = head;
-        } else {
-            tail->next = newMahasiswa;
-            tail = newMahasiswa;
-            newMahasiswa->next = head;
+    if (!head) {
+        head = newMahasiswa;
+        newMahasiswa->next = newMahasiswa;
+    } else {
+        Mahasiswa* last = head;
+        while (last->next != head) {
+            last = last->next;
         }
-        cout << "Mahasiswa " << nama << " ditambahkan ke dalam lingkaran." << endl;
+        last->next = newMahasiswa;
+        newMahasiswa->next = head;
     }
+}
 
-    void tambahMahasiswaDiTengah(const string& nama, char jenisKelamin) {
-        Mahasiswa* newMahasiswa = new Mahasiswa(nama, jenisKelamin);
+// Fungsi untuk menyisipkan mahasiswa ke dalam lingkaran pada posisi tertentu
+void sisipkanMahasiswa(Mahasiswa*& head, string nama, char jenisKelamin, int posisi) {
+    Mahasiswa* newMahasiswa = new Mahasiswa;
+    newMahasiswa->nama = nama;
+    newMahasiswa->jenisKelamin = jenisKelamin;
 
-        if (!head) {
-            head = newMahasiswa;
-            tail = newMahasiswa;
-            newMahasiswa->next = head;
-        } else {
-            int posisi;
-            cout << "Data dimasukkan pada urutan ke ";
-            cin >> posisi;
-
-            if (posisi < 1) {
-                newMahasiswa->next = head;
-                head = newMahasiswa;
-                tail->next = head;
-                posisi = 1;
-            } else {
-                Mahasiswa* current = head;
-                int counter = 1;
-
-                while (current->next != head && counter < posisi - 1) {
-                    current = current->next;
-                    counter++;
-                }
-
-                newMahasiswa->next = current->next;
-                current->next = newMahasiswa;
-
-                if (posisi > counter) {
-                    tail = newMahasiswa;
-                }
+    if (!head) {
+        head = newMahasiswa;
+        newMahasiswa->next = newMahasiswa;
+    } else {
+        if (posisi == 1) {
+            Mahasiswa* last = head;
+            while (last->next != head) {
+                last = last->next;
             }
+            last->next = newMahasiswa;
+            newMahasiswa->next = head;
+            head = newMahasiswa;
+        } else {
+            int counter = 1;
+            Mahasiswa* temp = head;
+            while (counter < posisi - 1 && temp->next != head) {
+                temp = temp->next;
+                counter++;
+            }
+            newMahasiswa->next = temp->next;
+            temp->next = newMahasiswa;
         }
+    }
+}
 
-        cout << "Berhasil\n";
+// Fungsi untuk menghapus mahasiswa dari lingkaran
+void hapusMahasiswa(Mahasiswa*& head, string nama) {
+    if (head == nullptr) {
+        return;
     }
 
-    // Menghapus mahasiswa dari lingkaran
-    void hapusMahasiswa() {
-        if (!head) {
-            cout << "Lingkaran kosong." << endl;
-            return;
-        }
+    Mahasiswa* current = head;
+    Mahasiswa* previous = nullptr;
 
-        string nama;
-        cout << "Masukkan nama mahasiswa yang ingin dihapus: ";
-        cin >> nama;
-
-        Mahasiswa* current = head;
-        Mahasiswa* prev = nullptr;
-
-        do {
-            if (current->nama == nama) {
-                if (current == head) {
-                    head = head->next;
-                    tail->next = head;
-                } else if (current == tail) {
-                    tail = prev;
-                    tail->next = head;
-                } else {
-                    prev->next = current->next;
+    while (current->next != head) {
+        if (current->nama == nama) {
+            if (current == head) {
+                Mahasiswa* last = head;
+                while (last->next != head) {
+                    last = last->next;
                 }
-
+                head = head->next;
+                last->next = head;
                 delete current;
-                cout << nama << " telah keluar dari lingkaran." << endl;
+                return;
+            } else {
+                previous->next = current->next;
+                delete current;
                 return;
             }
-
-            prev = current;
-            current = current->next;
-        } while (current != head);
-
-        cout << nama << " tidak ditemukan dalam lingkaran." << endl;
-    }
-
-    // Pisahkan mahasiswa berdasarkan jenis kelamin
-    void pisahkanJenisKelamin() {
-        if (!head) {
-            cout << "Lingkaran kosong." << endl;
-            return;
         }
 
-        Mahasiswa* current = head;
-        do {
-            if (current->jenisKelamin == 'P') {
-                cout << current->nama << " adalah mahasiswi." << endl;
-            } else {
-                cout << current->nama << " adalah mahasiswa." << endl;
-            }
-            current = current->next;
-        } while (current != head);
+        previous = current;
+        current = current->next;
     }
 
-    // Menampilkan daftar mahasiswa dalam lingkaran
-    void tampilkanMahasiswa() {
-        if (!head) {
-            cout << "Lingkaran kosong." << endl;
-            return;
+    // Check if the last node is the one to be deleted
+    if (current->nama == nama) {
+        if (current == head) {
+            head = nullptr;
+        } else {
+            previous->next = head;
         }
+        delete current;
+    }
+}
 
-        Mahasiswa* current = head;
-        cout << "Daftar Mahasiswa dalam Lingkaran [kiri = lebih dulu diinput]\n";
-        do {
-            cout << current->nama << " ";
-            current = current->next;
-        } while (current != head);
-        cout << endl;
+// Fungsi untuk majukan urutan bernyanyi
+Mahasiswa* lanjutkanBernyanyi(Mahasiswa* current) {
+    return current->next;
+}
+
+// Fungsi untuk menampilkan mahasiswa yang sedang bernyanyi
+void tampilkanMahasiswaYangBernyanyi(Mahasiswa* current) {
+    cout << "Mahasiswa yang sedang bernyanyi: " << current->nama << " (Jenis Kelamin: " << current->jenisKelamin << ")" << endl;
+}
+
+// Fungsi untuk menampilkan linked list mahasiswa
+void tampilkanLinkedList(Mahasiswa* head) {
+    if (head == nullptr) {
+        cout << "Lingkaran mahasiswa kosong." << endl;
+        return;
     }
 
-    // Memindah urutan bernyanyi
-    void pindahUrutanBernyanyi() {
-        if (!head) {
-            cout << "Lingkaran kosong." << endl;
-            return;
-        }
-
-        head = head->next; // Pindahkan kepala ke mahasiswa berikutnya
-        tampilkanMahasiswa();
-    }
-
-    // Tambahkan fungsi baru ke dalam kelas LingkaranMahasiswa
-    void tampilkanMahasiswaBerdasarkanJenisKelamin(char jenisKelamin) {
-        if (!head) {
-            cout << "Lingkaran kosong." << endl;
-            return;
-        }
-
-        Mahasiswa* current = head;
-        cout << "Daftar Mahasiswa ";
-        if (jenisKelamin == 'L') {
-            cout << "Laki-Laki";
-        } else if (jenisKelamin == 'P') {
-            cout << "Perempuan";
-        }
-        cout << ": ";
-
-        do {
-            if (current->jenisKelamin == jenisKelamin) {
-                cout << current->nama << " ";
-            }
-            current = current->next;
-        } while (current != head);
-        cout << endl;
-    }
-
-    // Mengambil mahasiswa yang sedang bernyanyi
-    string mahasiswaSedangBernyanyi() {
-        string alp;
-        if (!head) {
-            return "Lingkaran kosong.";
-        }
-
-        return head->nama;
-        cout << "Yang sedang bernyanyi adalah " << head -> nama;
-        cout << "Lanjut";
-        cin >> alp;
-        cout << endl;
-    }
-};
+    Mahasiswa* current = head;
+    do {
+        cout << "Mahasiswa: " << current->nama << " (Jenis Kelamin: " << current->jenisKelamin << ")" << endl;
+        current = current->next;
+    } while (current != head);
+}
 
 int main() {
-    LingkaranMahasiswa lingkaran;
+    Mahasiswa* lingkaranHead = nullptr;
+    Mahasiswa* mahasiswaYangSedangBernyanyi = nullptr;
+    vector<Mahasiswa> dataMahasiswa;
+
     int pilihan;
-    system("cls");
+    string nama;
+    char jenisKelamin;
 
     do {
-        cout << "|===============================|" << endl;
-        cout << "|    CAMPING DI GUNUNG ANYAR    |" << endl;
-        cout << "|===============================|" << endl;
-        cout << endl;
-        cout << "1. Tambah Mahasiswa di Depan" << endl;
-        cout << "2. Tambah Mahasiswa di Tengah" << endl;
-        cout << "3. Hapus Mahasiswa Berdasarkan Nama" << endl << endl;
-        cout << "4. Tampilkan Data Berdasarkan Kelamin" << endl;
-        cout << "5. Tampilkan Seluruh Isi List" << endl << endl;
-        cout << "6. Rolling Urutan Bernyanyi" << endl;
-        cout << "7. Mahasiswa yang Sedang Bernyanyi" << endl << endl;
-        cout << "8. Cetak Mahasiswa Laki-Laki" << endl;
-        cout << "9. Cetak Mahasiswa Perempuan" << endl;
-        cout << "0. Keluar" << endl << endl;
-        cout << "Pilih Menu ";
+        cout << "|=========================|" << endl;
+        cout << "| CAMPING DI GUNUNG ANYAR |" << endl;
+        cout << "|=========================|" << endl;
+
+        cout << "Menu:" << endl;
+        cout << "1. Tambah Mahasiswa ke Lingkaran" << endl;
+        cout << "2. Sisipkan Mahasiswa ke Tengah Linked List" << endl;
+        cout << "3. Hapus Mahasiswa dari Lingkaran" << endl;
+        cout << "4. Majukan Urutan Bernyanyi" << endl;
+        cout << "5. Tampilkan Mahasiswa yang Sedang Bernyanyi" << endl;
+        cout << "6. Tampilkan Linked List Mahasiswa" << endl;
+        cout << "7. Tampilkan Data Mahasiswa" << endl;
+        cout << "8. Keluar" << endl;
+        cout << "Pilihan: ";
         cin >> pilihan;
 
         switch (pilihan) {
             case 1:
-                lingkaran.tambahMahasiswa();
+                cout << "Masukkan nama mahasiswa: ";
+                cin >> nama;
+                cout << "Masukkan jenis kelamin (L/P): ";
+                cin >> jenisKelamin;
+                tambahMahasiswa(lingkaranHead, nama, jenisKelamin);
+                if (!mahasiswaYangSedangBernyanyi) {
+                    mahasiswaYangSedangBernyanyi = lingkaranHead;
+                }
+                dataMahasiswa.push_back({nama, jenisKelamin});
                 break;
             case 2:
-                {
-                    string nama;
-                    char jenisKelamin;
-
-                    cout << "Masukkan nama mahasiswa: ";
-                    cin >> nama;
-                    cout << "Masukkan jenis kelamin (L/P): ";
-                    cin >> jenisKelamin;
-
-                    lingkaran.tambahMahasiswaDiTengah(nama, jenisKelamin);
-                }
+                cout << "Masukkan nama mahasiswa: ";
+                cin >> nama;
+                cout << "Masukkan jenis kelamin (L/P): ";
+                cin >> jenisKelamin;
+                int posisi;
+                cout << "Masukkan posisi (nomer berapa) untuk menyisipkan: ";
+                cin >> posisi;
+                sisipkanMahasiswa(lingkaranHead, nama, jenisKelamin, posisi);
+                dataMahasiswa.push_back({nama, jenisKelamin});
                 break;
-
             case 3:
-                lingkaran.hapusMahasiswa();
+                cout << "Masukkan nama mahasiswa yang akan dihapus: ";
+                cin >> nama;
+                hapusMahasiswa(lingkaranHead, nama);
+                if (lingkaranHead) {
+                    mahasiswaYangSedangBernyanyi = lingkaranHead;
+                } else {
+                    mahasiswaYangSedangBernyanyi = nullptr;
+                }
+                dataMahasiswa.erase(
+                    remove_if(dataMahasiswa.begin(), dataMahasiswa.end(),
+                              [nama](const Mahasiswa& mhs) { return mhs.nama == nama; }),
+                    dataMahasiswa.end());
                 break;
             case 4:
-                lingkaran.pisahkanJenisKelamin();
+                if (mahasiswaYangSedangBernyanyi) {
+                    mahasiswaYangSedangBernyanyi = lanjutkanBernyanyi(mahasiswaYangSedangBernyanyi);
+                }
                 break;
             case 5:
-                lingkaran.tampilkanMahasiswa();
+                if (mahasiswaYangSedangBernyanyi) {
+                    tampilkanMahasiswaYangBernyanyi(mahasiswaYangSedangBernyanyi);
+                } else {
+                    cout << "Tidak ada mahasiswa yang sedang bernyanyi." << endl;
+                }
                 break;
             case 6:
-                lingkaran.pindahUrutanBernyanyi();
+                tampilkanLinkedList(lingkaranHead);
                 break;
             case 7:
-                cout << "Sedang bernyanyi: " << lingkaran.mahasiswaSedangBernyanyi() << endl;
+                if (dataMahasiswa.empty()) {
+                    cout << "Belum ada data mahasiswa." << endl;
+                } else {
+                    cout << "Data Mahasiswa:" << endl;
+                    for (const Mahasiswa& mhs : dataMahasiswa) {
+                        cout << "Nama: " << mhs.nama << " Jenis Kelamin: " << mhs.jenisKelamin << endl;
+                    }
+                }
                 break;
             case 8:
-                lingkaran.tampilkanMahasiswaBerdasarkanJenisKelamin('L');
+                cout << "Terima kasih!" << endl;
                 break;
-            case 9:
-                lingkaran.tampilkanMahasiswaBerdasarkanJenisKelamin('P');
-                break;
-            case 0:
-                cout << "Keluar dari program." << endl;
-                break;
-            default:;
+            default:
+                cout << "Pilihan tidak valid. Silakan coba lagi." << endl;
         }
-    } while (pilihan != 0);
+    } while (pilihan != 8);
+
+    while (lingkaranHead != nullptr) {
+        Mahasiswa* temp = lingkaranHead;
+        lingkaranHead = lingkaranHead->next;
+        delete temp;
+    }
 
     return 0;
 }
